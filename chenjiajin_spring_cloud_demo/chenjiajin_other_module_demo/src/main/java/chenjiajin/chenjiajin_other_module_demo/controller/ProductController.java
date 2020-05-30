77d8f2b6c20b677449e0a3c73a5189eeba17d816
陈嘉劲 <450883304@qq.com>
2020-05-30 16:28:14 +0800

    新建vue学习项目

--- chenjiajin_spring_cloud_demo/chenjiajin_other_module_demo/src/main/java/chenjiajin/chenjiajin_other_module_demo/controller/ProductController.java
+++ chenjiajin_spring_cloud_demo/chenjiajin_other_module_demo/src/main/java/chenjiajin/chenjiajin_other_module_demo/controller/ProductController.java
@@ -3,6 +3,9 @@ package chenjiajin.chenjiajin_other_module_demo.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("product")
@@ -15,6 +18,8 @@ public class ProductController {
         for (int i = 0; i < names.length; i++) {
             names[i] = "名字："+i;
         }
-        return names;
+        Map<String,Object> map = new HashMap<>();
+        map.put("2323",names);
+        return map;
     }
 }
