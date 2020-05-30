eda7ea77030fd062e7ecafc7aae90a0379308e8b
陈嘉劲 <450883304@qq.com>
2020-05-30 17:04:42 +0800

    创建跳转页面的路由

--- chenjiajin_whole_stack_demo/vue_demo/src/router/index.js
+++ chenjiajin_whole_stack_demo/vue_demo/src/router/index.js
@@ -1,11 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home/Home.vue'
+import CourseDetail from '../views/CourseDetail/CourseDetail.vue'
+import Login from '../views/Login/Login.vue'
+import Order from '../views/Order/Order.vue'
+import Pay from '../views/Pay/Pay.vue'
+import Personal from '../views/Personal/Personal.vue'
+import Register from '../views/Register/Register.vue'
 
 Vue.use(VueRouter)
 
 const routes = [{
-
-}
+    path: "/",
+    name: "Home",
+    component: Home
+},
+    {
+        path: "/courseDetail",
+        name: "CourseDetail",
+        //按需加载
+        component: ()=>import("../views/CourseDetail/CourseDetail.vue")
+        // component: CourseDetail
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login
+    },
+    {
+        path: "/order",
+        name: "Order",
+        component: Order
+    },
+    {
+        path: "/pay",
+        name: "Pay",
+        component: Pay
+    },
+    {
+        path: "/rersonal",
+        name: "Personal",
+        component: Personal
+    },
+    {
+        path: "/register",
+        name: "Register",
+        component: Register
+    },
 ]
 
 const router = new VueRouter({
